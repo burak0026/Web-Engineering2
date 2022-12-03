eeb7bcd3bc94e0a01d8996a6576f5a6c4839e504
MariusE1234 <84963436+MariusE1234@users.noreply.github.com>
2022-12-03 14:59:00 +0100

    update POST Statement
    POST reservations is now possible

--- compose/api-reservations/app.py
+++ compose/api-reservations/app.py
@@ -1,9 +1,6 @@
 import os
-from flask import Flask, jsonify, request
-
 import pgdb
-
-
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -24,18 +21,42 @@ def reservations_status():
 
 @app.route("/reservations/", methods=['GET', 'POST'])
 def reservations_general():
+    #open DB Connection and get cursor
+    conn = pgdb.connect(host="postgres", port = 5432, database="reservations", user="postgres", password="postgres")
+    cur = conn.cursor()
+    #POST Request
     if request.method == 'POST':
+        #get Values from Message Body
         content = request.json
-        return jsonify(response=content['id'])
+        #initialize variables
+        sqlStatement = ''
+        #input_id = content['id']
+        input_from = content['from']
+        input_to = content['to']
+        input_room_id = content['room_id']
+        #validate inputs
+        #-------implement------------
+        #create SQL statement
+        sqlStatement = "INSERT INTO reservations (\"from\", \"to\", room_id) VALUES ('"+ input_from +"', '"+ input_to +"', '"+ input_room_id +"')"
+        #execute SQL-Statement, make new Database entry
+        cur.execute(sqlStatement)
+        #close cursor, commit the SQL-Statement and close DB onnection
+        cur.close()
+        conn.commit()
+        conn.close()  
+        #return ok
+        return jsonify(
+            response='ok'
+        )
+    
+    #GET Request
     else:
-        conn = pgdb.connect(host="postgres", port = 5432, database="reservations", user="postgres", password="postgres")
-        cur = conn.cursor()
         cur.execute("SELECT * FROM reservations")
         query_results = cur.fetchall()
         cur.close()
         conn.close()
         return jsonify(query_results)
-            
+    
         
 @app.route("/reservations/<string>", methods=['GET'])
 def reservations_byID(string: str): 
