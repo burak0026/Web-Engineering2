6bfdbe6b058b246602521aecaa4c9c1bfea5dc64
MariusE1234 <84963436+MariusE1234@users.noreply.github.com>
2023-01-06 14:44:09 +0100

    make extra file for authorization code

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import json
 import datetime
 import uuid
 import requests
-import jwt
 from flask import Flask, jsonify, request, Response, make_response
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import UUID
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization
+
+from authorization import validate_jwt
+
 
 #create flask app and configure
 app = Flask(__name__)
@@ -94,29 +94,6 @@ def checkQueryValues(before, after, room_id):
 
 
 
-def validate_jwt(token):
-    if token is None:
-        app.logger.error('JWT not Found')
-        return Response("token not found", status=401)
-    #get public key from Keycloak
-    keycloak_url = f"http://{os.getenv('KEYCLOAK_HOST')}/auth/realms/{os.getenv('KEYCLOAK_REALM')}"
-    keycloak_request = requests.get(keycloak_url)
-    keycloak_request_json = json.loads(keycloak_request.text)
-    keycloak_pubkey = keycloak_request_json["public_key"]
-    #check if key is present, else throw 401
-    if keycloak_pubkey is None:
-        app.logger.error('Public key not Found')
-        return  Response("public key not found", status=401)
-    #add header/footer to public key and serialize
-    keycloak_pubkey = "-----BEGIN PUBLIC KEY-----\n"+keycloak_pubkey+"\n-----END PUBLIC KEY-----"
-    keycloak_pubkey = serialization.load_pem_public_key(bytes(keycloak_pubkey,'UTF-8'),default_backend)
-    #check if token is valid
-    try:
-        jwt.decode(token,keycloak_pubkey,algorithms=["RS256"],audience="account")
-        return True
-    except Exception:
-        app.logger.error('invalid Token')
-        return Response("invalid token", status=401)
 
 
 
